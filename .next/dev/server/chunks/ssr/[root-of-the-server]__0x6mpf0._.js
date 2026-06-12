d6db6b800317e33ab1035b70bdfa5d2041a7e4f3
ersetdigital-sudo <ersetdigital@gmail.com>
2026-06-12 10:59:43 +0700

    auto: update from Kiro

--- .next/dev/server/chunks/ssr/[root-of-the-server]__0x6mpf0._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__0x6mpf0._.js
@@ -377,9 +377,9 @@ function Navbar() {
                             className: "flex items-center",
                             "aria-label": "NexaPlus beranda",
                             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
-                                src: "/images/logo.png",
+                                src: "/images/logo.svg",
                                 alt: "NexaPlus",
-                                className: "h-8 w-auto"
+                                className: "h-7 w-auto"
                             }, void 0, false, {
                                 fileName: "[project]/components/layout/navbar.tsx",
                                 lineNumber: 54,
@@ -500,9 +500,9 @@ function Navbar() {
                                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                                 className: "flex items-center",
                                                 children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
-                                                    src: "/images/logo.png",
+                                                    src: "/images/logo.svg",
                                                     alt: "NexaPlus",
-                                                    className: "h-7 w-auto"
+                                                    className: "h-6 w-auto"
                                                 }, void 0, false, {
                                                     fileName: "[project]/components/layout/navbar.tsx",
                                                     lineNumber: 123,
--- .next/dev/static/chunks/_0uubqva._.js
+++ .next/dev/static/chunks/_0uubqva._.js
@@ -403,9 +403,9 @@ function Navbar() {
                             className: "flex items-center",
                             "aria-label": "NexaPlus beranda",
                             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
-                                src: "/images/logo.png",
+                                src: "/images/logo.svg",
                                 alt: "NexaPlus",
-                                className: "h-8 w-auto"
+                                className: "h-7 w-auto"
                             }, void 0, false, {
                                 fileName: "[project]/components/layout/navbar.tsx",
                                 lineNumber: 54,
@@ -526,9 +526,9 @@ function Navbar() {
                                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                                 className: "flex items-center",
                                                 children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
-                                                    src: "/images/logo.png",
+                                                    src: "/images/logo.svg",
                                                     alt: "NexaPlus",
-                                                    className: "h-7 w-auto"
+                                                    className: "h-6 w-auto"
                                                 }, void 0, false, {
                                                     fileName: "[project]/components/layout/navbar.tsx",
                                                     lineNumber: 123,
