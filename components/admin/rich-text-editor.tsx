e74b85efc71f109c61ae8c37c2e206e68500de73
ersetdigital-sudo <ersetdigital@gmail.com>
2026-06-13 12:35:56 +0700

    auto: update from Kiro

--- components/admin/rich-text-editor.tsx
+++ components/admin/rich-text-editor.tsx
@@ -4,10 +4,7 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
 import Link from "@tiptap/extension-link";
-import { Table } from "@tiptap/extension-table";
-import { TableRow } from "@tiptap/extension-table-row";
-import { TableCell } from "@tiptap/extension-table-cell";
-import { TableHeader } from "@tiptap/extension-table-header";
+import { Table, TableRow, TableCell, TableHeader } from "@tiptap/extension-table";
 import { useEffect } from "react";
 import {
   Bold,
