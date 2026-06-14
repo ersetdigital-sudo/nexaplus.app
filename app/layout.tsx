f1f89d6a9e1b2d2670ae808e75e7538ee9054e1a
ersetdigital-sudo <ersetdigital@gmail.com>
2026-06-14 11:19:37 +0700

    auto: update from Kiro

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,8 +76,6 @@ export default function RootLayout({
   return (
     <html lang="id" className={cn("font-sans", lato.variable)}>
       <head>
-        <link rel="preconnect" href="https://res.cloudinary.com" />
-        <link rel="dns-prefetch" href="https://res.cloudinary.com" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
